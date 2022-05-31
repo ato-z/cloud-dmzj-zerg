9c967de304c96586c96037225d2a3112ee2a85b9
atoz <523333404@qq.com>
2022-05-31 12:17:22 +0800

    添加容错机制

--- src/handler.ts
+++ src/handler.ts
@@ -1,8 +1,13 @@
 import { getBookListByParams } from "./core/get-list"
 
 export async function handleRequest(request: Request): Promise<Response> {
-  
-  const results = await getBookListByParams({})
-
-  return new Response(JSON.stringify(results))
+  try {
+    const results = await getBookListByParams({})
+    console.log(results)
+    return new Response(JSON.stringify(results))
+  } catch (err: unknown) {
+    let msg = '网络异常'
+    if (err instanceof Error) msg = err.message
+    return new Response(`{"msg": ${msg}, "error_code": 999}`)
+  }
 }
